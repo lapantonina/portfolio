f8091af303efa8225b01a4c672bbd13d27b4d7f6
Antonina Lapina <lapantonina@gmail.com>
2017-02-04 00:21:05 +0300

    переименована папка и изменены связ. файлы

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flickr_api.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_env.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
