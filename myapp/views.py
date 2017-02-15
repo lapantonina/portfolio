6173c1755be95d1eb90370034155e9f1086f54db
Antonina Lapina <lapantonina@gmail.com>
2017-02-15 05:26:24 +0300

    stack:   return TemplateResponse(request, '404.html')

--- myapp/views.py
+++ myapp/views.py
@@ -467,11 +467,8 @@ def stack(request):
       ask = 0.0
       bid = 0.0
     
-    print(exchange, ask_chain)
-
   lowest_ask = sorted(ask_chain.items(), key=lambda q: q[1]) #связки сортируются по значениям от мин. к макс. и наоборот
   highest_bid = sorted(bid_chain.items(), key=lambda q: q[1], reverse=True)
-
 
   spread = (highest_bid[0][1] - lowest_ask[0][1])
   pretty_spread = "%0.6f" % spread
@@ -490,8 +487,8 @@ def stack(request):
     )
   bet.save()
   
-  time.sleep(60)
-  return stack(request)
+  return TemplateResponse(request, '404.html')
+  response.status_code = 404
 
 
 def current_exchange_rate(request):
